31511e31fe5c350628380f2ced88d9edabec37be
jonatas-campista <jonatasc5622@gmail.com>
2024-10-24 17:05:19 -0300

    Submitting an image with invalid inputs

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,4 +1,4 @@
-class registerForm {
+class RegisterForm {
   elements = {
     titleInput: () => cy.get('#title'),
     titleFeedBack: () => cy.get('#titleFeedback'),
@@ -6,11 +6,34 @@ class registerForm {
     imageFeedBack: () => cy.get('#urlFeedback'),
     submitBtn: () => cy.get('#btnSubmit')
   }
+
+  tipeTitle(text) {
+    if (!text) return ;
+    this.elements.titleInput().type(text);
+  }
+
+  tipeUrl(text) {
+    if (!text) return ;
+    this.elements.imageUrlInput().type(text);
+  }
+  clickSubmit() {
+    this.elements.submitBtn().click();
+  }
+}
+
+const registerForm = new RegisterForm();
+
+const colors ={
+  errors: 'rgb(220, 53, 69)',
+  sucess: ''
 }
 
 describe('Image Registration', () => {
 
   describe('Submitting an image with invalid inputs', () => {
+    after(() => {
+      cy.clearAllLocalStorage()
+    });
     const input = {
       title: '',
       url: ''
@@ -20,26 +43,28 @@ describe('Image Registration', () => {
     })
 
     it(`When I enter "${input.title}" in the title field`, () => {
+      registerForm.tipeTitle(input.title);
     });
 
     it(`Then I enter ${input.url} in the URL field`, () => {
-      
+      registerForm.tipeUrl(input.url);
     });
 
     it('Then I click the submit button', () => {
-      
+      registerForm.clickSubmit();
     });
 
     it('Then I should see "Please type a title for the image" message above the title field', () => {
-      
+      registerForm.elements.titleFeedBack().should('contain.text','Please type a title for the image')
     });
 
     it('And I should see "Please type a valid URL" message above the imageUrl field', () => {
-      
+      registerForm.elements.imageFeedBack().should('contain.text','Please type a valid URL')
     });
 
     it('And I should see an exclamation icon in the title and URL fields', () => {
-      
+      registerForm.elements.titleInput().should('have.css','border-color',colors.errors)
+      registerForm.elements.imageUrlInput().should('have.css','border-color',colors.errors)
     });
   });
 })
